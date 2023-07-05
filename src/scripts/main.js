3aedf44a458301f95a0094068a5ed26025705542
Khnykin-Artem <k98030118@gmail.com>
2023-07-05 21:05:39 +0300

    header

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,5 +5,17 @@ isWebp();
 $(() => {
   $('.header-slider').slick({
     infinite: true,
+    fade: true,
+    prevArrow:
+      '<img class="slider-arrows slider-arrows-left" src="images/arrows-left.svg" alt="arrows-right">',
+    nextArrow:
+      '<img class="slider-arrows slider-arrows-right" src="images/arrows-right.svg" alt="arrows-right">',
+    asNavFor: '.slider-dots',
+  });
+
+  $('.slider-dots').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    asNavFor: '.header-slider',
   });
 });
