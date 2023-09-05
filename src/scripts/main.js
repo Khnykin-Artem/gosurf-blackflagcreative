3a3564f8e04adc0f6aeff40174ecfed244d00937
Khnykin-Artem <k98030118@gmail.com>
2023-09-05 18:57:11 +0300

    develop holder

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -44,9 +44,9 @@ $(() => {
   });
 
   $(
-    '<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>'
+    '<div class="quantity-nav"><div class="quantity-button quantity-up"><img src="images/plus.svg"></div><div class="quantity-button quantity-down"><img src="images/minus.svg"></div></div>'
   ).insertAfter('.quantity input');
-  $('.quantity').each(() => {
+  $('.quantity').each(function () {
     const spinner = $(this);
     const input = spinner.find('input[type="number"]');
     const btnUp = spinner.find('.quantity-up');
@@ -78,36 +78,4 @@ $(() => {
       spinner.find('input').trigger('change');
     });
   });
-  // jQuery('<div class="quantity-nav"><div class="quantity-button quantity-up">+</div><div class="quantity-button quantity-down">-</div></div>').insertAfter('.quantity input');
-  //   jQuery('.quantity').each(function() {
-  //     var spinner = jQuery(this),
-  //       input = spinner.find('input[type="number"]'),
-  //       btnUp = spinner.find('.quantity-up'),
-  //       btnDown = spinner.find('.quantity-down'),
-  //       min = input.attr('min'),
-  //       max = input.attr('max');
-
-  //     btnUp.click(function() {
-  //       var oldValue = parseFloat(input.val());
-  //       if (oldValue >= max) {
-  //         var newVal = oldValue;
-  //       } else {
-  //         var newVal = oldValue + 1;
-  //       }
-  //       spinner.find("input").val(newVal);
-  //       spinner.find("input").trigger("change");
-  //     });
-
-  //     btnDown.click(function() {
-  //       var oldValue = parseFloat(input.val());
-  //       if (oldValue <= min) {
-  //         var newVal = oldValue;
-  //       } else {
-  //         var newVal = oldValue - 1;
-  //       }
-  //       spinner.find("input").val(newVal);
-  //       spinner.find("input").trigger("change");
-  //     });
-
-  //   });
 });
